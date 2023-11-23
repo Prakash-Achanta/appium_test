89fd083e21510a213a421113754d38c25f971b13
achantaprakash <118893794+Prakash-Achanta@users.noreply.github.com>
2023-11-23 21:28:39 +0530

    Initial Commit

---
+++ src/test/java/org/test/DemoApp.java
@@ -0,0 +1,27 @@
+package org.test;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.remote.AutomationName;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class DemoApp {
+
+    @Test
+    public void lauchTest() throws MalformedURLException {
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setPlatformName("Android");
+        options.setAutomationName(AutomationName.ANDROID_UIAUTOMATOR2);
+        options.setDeviceName("RMX-1825");
+        options.setApp(System.getProperty("user.dir")+"/apps/Android-MyDemoAppRN.1.3.0.build-244.apk");
+
+        AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),options);
+        driver.findElement(AppiumBy.accessibilityId("open menu")).click();
+        driver.findElement(By.xpath(("//android.view.ViewGroup[@content-desc='menu item log in']"))).click();
+    }
+}
